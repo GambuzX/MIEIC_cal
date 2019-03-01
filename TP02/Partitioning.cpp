f37c40807300eab72f59a546b36a7430ca9cecb1
Mário Gil <mg_mesquita@hotmail.com>
2019-03-01 18:50:52 +0000

    Solved Partitioning exercise

--- TP02/Partitioning.cpp
+++ TP02/Partitioning.cpp
@@ -3,27 +3,62 @@
  */
 
 #include "Partitioning.h"
+#include <string.h>
 
 
 int s_recursive(int n,int k)
 {
-   return 1;
+	if (k > n) return 0; // ERROR
+	if (k == 1 || k == n) return 1;
+	return s_recursive(n-1, k-1) + k * s_recursive(n-1, k);
 }
 
 int s_dynamic(int n,int k)
 {
-	return 1;
+	int pre_calc[n+1][k+1];
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= k; j++) {
+			if (j > i) continue;
+
+			if (j == 1 || j == i)
+				pre_calc[i][j] = 1;
+			else
+				pre_calc[i][j] = pre_calc[i-1][j-1] + j * pre_calc[i-1][j];
+		}
+	}
+
+	return pre_calc[n][k];
 }
 
 
 int b_recursive(int n)
 {
-	return 0;
+	int b = 0;
+	for (int k = 1; k <= n; k++) b += s_recursive(n, k);
+	return b;
 }
 
 int b_dynamic(int n)
 {
-	return 0;
+	int pre_calc[n+1][n+1];
+	memset(pre_calc, 0, sizeof(pre_calc));
+
+	for (int k = 1; k <= n; k++) {
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= k; j++) {
+				if (j > i || pre_calc[i][j] != 0) continue;
+
+				if (j == 1 || j == i)
+					pre_calc[i][j] = 1;
+				else
+					pre_calc[i][j] = pre_calc[i-1][j-1] + j * pre_calc[i-1][j];
+			}
+		}
+	}
+
+	int sum = 0;
+	for (int k = 1; k <= n; k++) sum += pre_calc[n][k];
+	return sum;
 }
 
 
